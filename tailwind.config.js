39b854b06b9b783827f8069be71a10202f362370
chayan999 <chayanchandandas@gmail.com>
2024-09-14 16:58:54 +0600

    add toDeskTop setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'display':["Poppins", 'sans-serif'],
+        'body':["Inter",' sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
